d208f50c8b43f31c52b0a568eb4dca604d96300d
benyates786 <92039020+benyates786@users.noreply.github.com>
2021-11-05 16:50:32 +0500

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,8 +6,11 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 
 app = flask.Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['MONGO_URI'] = 'mongodb+srv://ben:Pakchk123@cluster0.9yvjr.mongodb.net/movie?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+#app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+#app.config['MONGO_URI'] = 'mongodb+srv://ben:Pakchk123@cluster0.9yvjr.mongodb.net/movie?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+
+app.config['SECRET_KEY'] = 'process.env.SECRET_KEY'
+app.config['MONGO_URI'] = 'process.env.MONGO_URI'
 mongo = PyMongo(app)
 
 bcrypt = Bcrypt(app)
